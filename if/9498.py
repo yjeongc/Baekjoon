71cc5420545a6b966bdcfcba835ddfcdcaa4562a
Yujeong <yjeng34@duksung.ac.kr>
2022-05-31 23:54:15 +0900

    Create 9498.py

---
+++ if/9498.py
@@ -0,0 +1,16 @@
+S=int(input())
+
+if (S>=90) and (S<=100):
+    print("A")
+
+elif (S>=80) and (S<=89):
+    print("B")
+
+elif (S>=70) and (S<=79):
+    print("C")
+
+elif (S>=60) and (S<=69):
+    print("D")
+
+else:
+    print("F")
